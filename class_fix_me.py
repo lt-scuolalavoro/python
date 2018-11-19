0c0fffccb8963ee903106e205f5590e771bd16c4
Francesco Urbano <scrivimi@francescourbano.it>
2018-11-19 13:55:22 +0100

    Create class_fix_me.py
    Example of class definition and usage in Python

---
+++ class_fix_me.py
@@ -0,0 +1,16 @@
+def phraseClass:
+  phrase = ""
+  def invert(self)
+    return self.phrase[::-1]
+  def wordCount():
+     return len(self.phrase.split())
+
+phrase1 = phraseClass()
+phrase1.phrase = "Hello World!"
+print(phrase1.invert())
+print(phrase1.wordcount())
+
+phrase2 = phraseClass
+phrase2.phrase = "All good things come to an end"
+print(phrase2.invert())
+print(phrase2.wordcount())
