2bb653b6ecdfffa861e296a79a493b1113857eac
Giuseppe Lepore <giuseppejoelepore@gmail.com>
2018-11-19 15:35:34 +0100

    Fix errors

--- class_fix_me.py
+++ class_fix_me.py
@@ -1,16 +1,16 @@
-def phraseClass:
+class phraseClass:
   phrase = ""
-  def invert(self)
+  def invert(self):
     return self.phrase[::-1]
-  def wordCount():
+  def wordCount(self):
      return len(self.phrase.split())
 
 phrase1 = phraseClass()
 phrase1.phrase = "Hello World!"
 print(phrase1.invert())
-print(phrase1.wordcount())
+print(phrase1.wordCount())
 
-phrase2 = phraseClass
+phrase2 = phraseClass()
 phrase2.phrase = "All good things come to an end"
 print(phrase2.invert())
-print(phrase2.wordcount())
+print(phrase2.wordCount())
